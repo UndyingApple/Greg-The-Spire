0190492dc53e00f54077e478741bd1e4573f76d4
sniveeey <thaddeusf1208@gmail.com>
2026-08-27 21:02:36 -0500

    Added Cooking Pot, Emergency Rations, Seconds, and Soup

---
+++ GregTheSpireCode/Cards/CookingPot.cs
@@ -0,0 +1,36 @@
+﻿using GregTheSpire.GregTheSpireCode.Cards;
+using GregTheSpire.GregTheSpireCode.Cards.Colorless;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Commands.Builders;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace GregTheSpire.GregTheSpireCode.Cards;
+
+public class CookingPot() : GregTheSpireCard(2,
+    CardType.Attack, CardRarity.Rare,
+    TargetType.AnyEnemy)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new DamageVar(9, ValueProp.Move),
+        new BlockVar(9, ValueProp.Move)
+    ];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay cardPlay)
+    {
+        Decimal num = await CreatureCmd.GainBlock(this.Owner.Creature, this.DynamicVars.Block, cardPlay);
+        AttackCommand attackCommand = await DamageCmd.Attack(this.DynamicVars.Damage.BaseValue).FromCard((CardModel) this, cardPlay).Targeting(cardPlay.Target).Execute(choiceContext);
+        IEnumerable<CardModel> soup = await Soup.CreateInHand(Owner, 1, CombatState);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(3);
+        DynamicVars.Block.UpgradeValueBy(3);
+    }
+}
---
+++ GregTheSpireCode/Cards/Seconds.cs
@@ -0,0 +1,32 @@
+﻿using GregTheSpire.GregTheSpireCode.Cards;
+using GregTheSpire.GregTheSpireCode.Cards.Colorless;
+using GregTheSpire.GregTheSpireCode.Powers;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+
+namespace GregTheSpire.GregTheSpireCode.Cards;
+
+public class Seconds() : GregTheSpireCard(1,
+    CardType.Skill, CardRarity.Common,
+    TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [];
+
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [
+        CardKeyword.Exhaust
+    ];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        await CreatureCmd.TriggerAnim(Owner.Creature, "Cast", Owner.Character.CastAnimDelay);
+        IEnumerable<CardModel> cracker = await Cracker.CreateInHand(Owner, 1, CombatState);
+        SecondsPower secondsPower = await PowerCmd.Apply<SecondsPower>(choiceContext, Owner.Creature, 1, Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade() => RemoveKeyword(CardKeyword.Exhaust);
+}
