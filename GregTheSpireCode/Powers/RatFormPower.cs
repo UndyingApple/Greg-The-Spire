59cd428037bc5ada5473ac002defae447d432ee6
sockswithafox <sockswithafox@gmail.com>
2026-08-29 13:20:01 -0500

    evil rat form

---
+++ GregTheSpireCode/Powers/RatFormPower.cs
@@ -0,0 +1,37 @@
+﻿
+using MegaCrit.Sts2.Core.Commands;
+
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Powers;
+
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+
+namespace GregTheSpire.GregTheSpireCode.Powers;
+
+
+public class RatFormPower() : GregTheSpirePower
+{
+    public override PowerType Type =>
+        PowerType.Buff;
+
+    public override PowerStackType StackType =>
+        PowerStackType.Single;
+   
+
+    public override async Task AfterAutoPrePlayPhaseEntered(
+        PlayerChoiceContext choiceContext,
+        Player player)
+    {
+        
+        var flag = true;
+        
+        foreach (CardModel card in this.Owner.Player.PlayerCombatState.none.Cards)
+                    
+        await CardCmd.AutoPlay(choiceContext, card, (Creature)null, skipCardPileVisuals: !flag);
+        flag = false;
+    }
+
+}
+
