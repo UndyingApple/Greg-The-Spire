ef9cf2fcc5a3457bb8a7250607f519dfd8962b97
sniveeey <thaddeusf1208@gmail.com>
2026-08-31 15:04:42 -0500

    Added Sugar Rush

--- GregTheSpireCode/Cards/Hardtack.cs
+++ GregTheSpireCode/Cards/Hardtack.cs
@@ -6,6 +6,7 @@ using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Commands.Builders;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.ValueProps;
@@ -24,6 +25,11 @@ public class Hardtack() : GregTheSpireCard(1,
             CombatManager.Instance.History.CardPlaysStarted
                 .Count(e => e.CardPlay.Card.Owner == card.Owner
                             && e.CardPlay.Card is Cracker))
+    ];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        HoverTipFactory.FromCard<Cracker>()
     ];
 
     protected override async Task OnPlay(
---
+++ GregTheSpireCode/Cards/SugarRush.cs
@@ -0,0 +1,46 @@
+﻿using GregTheSpire.GregTheSpireCode.Cards;
+using GregTheSpire.GregTheSpireCode.Keywords;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Combat.History.Entries;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Commands.Builders;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace GregTheSpire.GregTheSpireCode.Cards;
+
+public class SugarRush() : GregTheSpireCard(1,
+    CardType.Attack, CardRarity.Uncommon,
+    TargetType.AnyEnemy)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new DamageVar(5, ValueProp.Move),
+        new CalculationBaseVar(0),
+        new CalculationExtraVar(1),
+        new CalculatedVar("CalculatedHits").WithMultiplier((Func<CardModel, Creature, Decimal>) ((card, _) => 
+            (Decimal) CombatManager.Instance.History.Entries.OfType<CardPlayFinishedEntry>()
+                .Count<CardPlayFinishedEntry>((Func<CardPlayFinishedEntry, bool>) (e => e.CardPlay.Player == card.Owner
+                    && e.CardPlay.Card.Keywords.Contains(GregTheSpireKeywords.Snack)
+                    && e.HappenedThisTurn(card.CombatState)))))
+    ];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        HoverTipFactory.FromKeyword(GregTheSpireKeywords.Snack)
+    ];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        ArgumentNullException.ThrowIfNull((object) play.Target, "play.Target");
+        AttackCommand attackCommand = await DamageCmd.Attack(this.DynamicVars.Damage.BaseValue).WithHitCount((int) ((CalculatedVar) this.DynamicVars["CalculatedHits"]).Calculate(play.Target)).FromCard((CardModel) this, play).Targeting(play.Target).Execute(choiceContext);
+    }
+
+    protected override void OnUpgrade() => this.DynamicVars.Damage.UpgradeValueBy(2M);
+}
