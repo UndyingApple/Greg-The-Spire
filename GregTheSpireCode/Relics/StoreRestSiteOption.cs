a99b879ac33005630978be91ea54e2c3f36741be
sockswithafox <sockswithafox@gmail.com>
2026-09-02 12:14:38 -0500

    IT WORKS

--- GregTheSpireCode/Relics/StoreRestSiteOption.cs
+++ GregTheSpireCode/Relics/StoreRestSiteOption.cs
@@ -1,4 +1,5 @@
-﻿using Godot;
+﻿using System.Reflection;
+using Godot;
 using GregTheSpire.GregTheSpireCode.Enchantments;
 using GregTheSpire.GregTheSpireCode.Extensions;
 using MegaCrit.Sts2.Core.CardSelection;
@@ -23,25 +24,20 @@ namespace GregTheSpire.GregTheSpireCode.Relics;
 public class StoreRestSiteOption(Player owner) : RestSiteOption(owner)
 {
 
+  private static readonly FieldInfo ChoicesScreenField =
+    typeof(NRestSiteRoom).GetField("_choicesScreen", BindingFlags.NonPublic | BindingFlags.Instance)!;
 
-  public override string OptionId => "GREGTHESPIRE_STORE";
-  
-  public string CustomIconPath => "rest_site/store.png".ImagePath();
+  public const string StoreOptionId = "GREGTHESPIRE_STORE";
 
-  public int Cards { get; set; } = 1;
+  public override string OptionId => StoreOptionId;
 
-  public override LocString Description
-  {
-    get
-    {
-      if (!this.IsEnabled)
-        return new LocString("rest_site_ui", $"OPTION_{this.OptionId}.descriptionDisabled");
-      LocString description = new LocString("rest_site_ui", $"OPTION_{this.OptionId}.description");
-      description.Add("Cards", 1);
-      return description;
-    }
-  }
-   public override bool IsEnabled => 3 >= 2;
+  public override LocString Description =>
+    new LocString("rest_site_ui", "OPTION_" + OptionId + ".description");
+
+
+
+  public override IEnumerable<string> AssetPaths => Array.Empty<string>();
+
 
    public override async Task<bool> OnSelect()
    {
