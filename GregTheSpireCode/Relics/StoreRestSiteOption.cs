88131e2896592449f648af98d843df3787eb3603
sockswithafox <sockswithafox@gmail.com>
2026-09-02 11:24:37 -0500

    localizatio fixed

--- GregTheSpireCode/Relics/StoreRestSiteOption.cs
+++ GregTheSpireCode/Relics/StoreRestSiteOption.cs
@@ -1,5 +1,6 @@
 ﻿using Godot;
 using GregTheSpire.GregTheSpireCode.Enchantments;
+using GregTheSpire.GregTheSpireCode.Extensions;
 using MegaCrit.Sts2.Core.CardSelection;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Context;
@@ -21,30 +22,37 @@ namespace GregTheSpire.GregTheSpireCode.Relics;
 
 public class StoreRestSiteOption(Player owner) : RestSiteOption(owner)
 {
-  private IEnumerable<CardModel>? _selection;
-
-  public override string OptionId => "STORE";
 
 
-  public int StoreCount { get; set; } = 1;
+  public override string OptionId => "GREGTHESPIRE_STORE";
+  
+  public string CustomIconPath => "rest_site/store.png".ImagePath();
 
-   public override LocString Description
-   {
-     get
-     {
-       if (!this.IsEnabled)
-         return new LocString("rest_site_ui", $"OPTION_{this.OptionId}.descriptionDisabled");
-       LocString description = new LocString("rest_site_ui", $"OPTION_{this.OptionId}.description");
-       description.Add("Cards", 1);
-       return description;
-     }
-   }
+  public int Cards { get; set; } = 1;
+
+  public override LocString Description
+  {
+    get
+    {
+      if (!this.IsEnabled)
+        return new LocString("rest_site_ui", $"OPTION_{this.OptionId}.descriptionDisabled");
+      LocString description = new LocString("rest_site_ui", $"OPTION_{this.OptionId}.description");
+      description.Add("Cards", 1);
+      return description;
+    }
+  }
    public override bool IsEnabled => 3 >= 2;
 
    public override async Task<bool> OnSelect()
    {
-     CardSelectorPrefs prefs = new CardSelectorPrefs(CardSelectorPrefs.EnchantSelectionPrompt, 1);
-     IEnumerable<CardModel> source = await CardSelectCmd.FromDeckForEnchantment(this.Owner,(EnchantmentModel) ModelDb.Enchantment<Stowaway>(), 1, prefs);
+     CardSelectorPrefs prefs = new CardSelectorPrefs(CardSelectorPrefs.EnchantSelectionPrompt, 1)
+     {
+       Cancelable = true,
+       RequireManualConfirmation = true
+     };
+     IEnumerable<CardModel> source = await CardSelectCmd.FromDeckForEnchantment(this.Owner,
+       (EnchantmentModel)ModelDb.Enchantment<Stowaway>(), 1, prefs);
+
      
      if (!source.Any<CardModel>())
        return false;
