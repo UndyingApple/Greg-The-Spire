a17ab602e2dc2623f540636673652efbe370b7a6
UndyingApple <undyingapple@gmail.com>
2026-09-01 20:17:53 -0500

    added grappling hook and tail swipe

---
+++ GregTheSpireCode/Cards/GrapplingHook.cs
@@ -0,0 +1,41 @@
+using BaseLib.Extensions;
+using GregTheSpire.GregTheSpireCode.Cards;
+using GregTheSpire.GregTheSpireCode.Commands;
+using GregTheSpire.GregTheSpireCode.Powers;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Combat.History.Entries;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+
+namespace GregTheSpire.GregTheSpireCode.Cards;
+
+public class GrapplingHook() : GregTheSpireCard(0,
+    CardType.Skill, CardRarity.Rare,
+    TargetType.Self)
+{
+    
+    private bool HasBeenPlayedThisTurn
+    {
+        get
+        {
+            return CombatManager.Instance.History.CardPlaysFinished.Any<CardPlayFinishedEntry>((Func<CardPlayFinishedEntry, bool>) (e => e.CardPlay.Card == this && e.HappenedThisTurn(this.CombatState)));
+        }
+    }
+    
+    protected override bool ShouldGlowGoldInternal => !this.HasBeenPlayedThisTurn;
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars => [];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        if (this.HasBeenPlayedThisTurn)
+            return;
+        await StealCmd.StealAsync(choiceContext, this.Owner, 1);
+    }
+
+    protected override void OnUpgrade() => this.AddKeyword(CardKeyword.Innate);
+}
---
+++ GregTheSpireCode/Cards/TailSwipe.cs
@@ -0,0 +1,32 @@
+using GregTheSpire.GregTheSpireCode.CardPiles;
+using GregTheSpire.GregTheSpireCode.Cards;
+using GregTheSpire.GregTheSpireCode.Keywords;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace GregTheSpire.GregTheSpireCode.Cards;
+
+public class TailSwipe() : GregTheSpireCard(1,
+    CardType.Attack, CardRarity.Common,
+    TargetType.AllEnemies)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [ new DamageVar(4, ValueProp.Move)];
+    
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        await CreatureCmd.TriggerAnim(this.Owner.Creature, "Attack", this.Owner.Character.AttackAnimDelay);
+        await DamageCmd.Attack(this.DynamicVars.Damage.BaseValue).FromCard((CardModel) this, play).TargetingAllOpponents(this.CombatState).WithHitFx("vfx/vfx_attack_slash").Execute(choiceContext);
+        await CardPileCmd.Draw(choiceContext, StashCardPile.StashPileType.GetPile(this.Owner).Cards.Count, this.Owner, false);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(3);
+    }
+}
