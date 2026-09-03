a8c84d802b6c82c39e4d84e3a4b3c00d1459fa9d
sniveeey <thaddeusf1208@gmail.com>
2026-09-02 22:28:52 -0500

    Added Improvised Armor

---
+++ GregTheSpireCode/Cards/ImprovisedArmor.cs
@@ -0,0 +1,41 @@
+﻿using GregTheSpire.GregTheSpireCode.CardPiles;
+using GregTheSpire.GregTheSpireCode.Cards;
+using GregTheSpire.GregTheSpireCode.Keywords;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace GregTheSpire.GregTheSpireCode.Cards;
+
+public class ImprovisedArmor() : GregTheSpireCard(2,
+    CardType.Skill, CardRarity.Basic,
+    TargetType.Self)
+{
+    public override bool GainsBlock => true;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new CalculationBaseVar(0),
+        new CalculationExtraVar(4),
+        new CalculatedBlockVar(ValueProp.Move).WithMultiplier((Func<CardModel, Creature, Decimal>) ((card, _) => (Decimal) StashCardPile.StashPileType.GetPile(card.Owner).Cards.Count))
+    ];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        HoverTipFactory.FromKeyword(GregTheSpireKeywords.Stash)
+    ];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        await CreatureCmd.TriggerAnim(this.Owner.Creature, "Cast", this.Owner.Character.CastAnimDelay);
+        await CreatureCmd.GainBlock(this.Owner.Creature, this.DynamicVars.CalculatedBlock.Calculate(play.Target), this.DynamicVars.CalculatedBlock.Props, play);
+    }
+
+    protected override void OnUpgrade() => DynamicVars.CalculationExtra.UpgradeValueBy(2);
+}
