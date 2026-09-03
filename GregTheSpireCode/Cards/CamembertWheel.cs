410c655513b1f0ff2f12353c3fd5c02031970f5b
sniveeey <thaddeusf1208@gmail.com>
2026-09-03 14:22:10 -0500

    Added Camembert Wheel (might be buggy, as there's not enough testing)

---
+++ GregTheSpireCode/Cards/CamembertWheel.cs
@@ -0,0 +1,105 @@
+﻿using BaseLib.Extensions;
+using GregTheSpire.GregTheSpireCode.Cards;
+using GregTheSpire.GregTheSpireCode.Cards.Colorless;
+using GregTheSpire.GregTheSpireCode.Keywords;
+using GregTheSpire.GregTheSpireCode.Powers;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Commands.Builders;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Hooks;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Cards;
+using MegaCrit.Sts2.Core.Saves.Runs;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace GregTheSpire.GregTheSpireCode.Cards;
+
+public class CamembertWheel() : GregTheSpireCard(1,
+    CardType.Attack, CardRarity.Rare,
+    TargetType.AllEnemies)
+{
+    private int _currentDamage = 2;
+    private int _increasedDamage;
+    private bool hasPlayedCheeseThisTurn = false;
+    
+    [SavedProperty]
+    public int CurrentDamage
+    {
+        get => this._currentDamage;
+        set
+        {
+            this.AssertMutable();
+            this._currentDamage = value;
+            this.DynamicVars.Damage.BaseValue = (Decimal) this._currentDamage;
+        }
+    }
+
+    [SavedProperty]
+    public int IncreasedDamage
+    {
+        get => this._increasedDamage;
+        set
+        {
+            this.AssertMutable();
+            this._increasedDamage = value;
+        }
+    }
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new DamageVar(2, ValueProp.Move),
+        new IntVar("Increase", 1)
+    ];
+
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [
+        GregTheSpireKeywords.Snack
+    ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        Decimal damage = Hook.ModifyDamage(this.RunState, this.CombatState, cardPlay.Target, this.Owner.Creature, DynamicVars.Damage.BaseValue, DynamicVars.Damage.Props, (CardModel) this, cardPlay, ModifyDamageHookType.All, CardPreviewMode.MultiCreatureTargeting, out IEnumerable<AbstractModel> _);
+        await PowerCmd.Apply<CamembertWheelPower>(choiceContext, Owner.Creature, damage, Owner.Creature, this);
+    }
+
+    public override Task AfterCardPlayed(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        if (cardPlay.Card is Cheese && !hasPlayedCheeseThisTurn) {
+            hasPlayedCheeseThisTurn = true;
+            int intValue = DynamicVars["Increase"].IntValue;
+            this.BuffFromPlay(intValue);
+            if (this.DeckVersion is CamembertWheel deckVersion)
+            {
+                deckVersion.BuffFromPlay(intValue);
+            }
+        }
+        return Task.CompletedTask;
+    }
+
+    public override Task AfterSideTurnEnd(PlayerChoiceContext choiceContext, CombatSide side, IEnumerable<Creature> participants)
+    {
+        if (side == CombatSide.Player)
+        {
+            hasPlayedCheeseThisTurn = false;
+        }
+        return Task.CompletedTask;
+    }
+
+    protected override void OnUpgrade()
+    {
+        this.DynamicVars["Increase"].UpgradeValueBy(1);
+        this.UpdateDamage();
+    }
+
+    protected override void AfterDowngraded() => this.UpdateDamage();
+
+    public void BuffFromPlay(int extraDamage)
+    {
+        this.IncreasedDamage += extraDamage;
+        this.UpdateDamage();
+    }
+
+    private void UpdateDamage() => this.CurrentDamage = (int) DynamicVars.Damage.BaseValue + this.IncreasedDamage;
+}
---
+++ GregTheSpireCode/Powers/CamembertWheelPower.cs
@@ -0,0 +1,59 @@
+﻿using Godot;
+using GregTheSpire.GregTheSpireCode.Powers;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Nodes.Combat;
+using MegaCrit.Sts2.Core.Nodes.Rooms;
+using MegaCrit.Sts2.Core.Nodes.Vfx;
+using MegaCrit.Sts2.Core.TestSupport;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace GregTheSpire.GregTheSpireCode.Powers;
+
+public class CamembertWheelPower() : GregTheSpirePower
+{
+    public override PowerType Type =>
+        PowerType.Buff;
+
+    public override PowerStackType StackType =>
+        PowerStackType.Counter;
+
+    public override PowerInstanceType InstanceType =>
+        PowerInstanceType.Instanced;
+    
+    public override async Task AfterPlayerTurnStart(PlayerChoiceContext choiceContext, Player player)
+    {
+        if (player != this.Owner.Player)
+            ;
+        else
+        {
+            this.Flash();
+            if (TestMode.IsOn)
+            {
+                IEnumerable<DamageResult> damageResults = await this.DoDamage(choiceContext, (IEnumerable<Creature>) this.CombatState.HittableEnemies);
+            }
+            else
+            {
+                List<Task> damageTasks = new List<Task>();
+                NRollingBoulderVfx source = NRollingBoulderVfx.Create((IEnumerable<Creature>) this.CombatState.HittableEnemies, (Decimal) this.Amount);
+                // ISSUE: object of a compiler-generated type is created
+                long num = (long) source.Connect(NRollingBoulderVfx.SignalName.HitCreature, Callable.From<NCreature>((Action<NCreature>) (c => damageTasks.Add((Task) this.DoDamage(choiceContext, [c.Entity])))));
+                SignalAwaiter signal = source.ToSignal((GodotObject) source, NRollingBoulderVfx.SignalName.Finished);
+                NCombatRoom.Instance?.CombatVfxContainer.CallDeferred(Node.MethodName.AddChild, (Variant) (GodotObject) source);
+                Variant[] variantArray = await signal;
+                await Task.WhenAll((IEnumerable<Task>) damageTasks);
+            }
+            await PowerCmd.Remove(this);
+        }
+    }
+
+    private Task<IEnumerable<DamageResult>> DoDamage(
+        PlayerChoiceContext choiceContext,
+        IEnumerable<Creature> targets)
+    {
+        return CreatureCmd.Damage(choiceContext, targets, (Decimal) this.Amount, ValueProp.Unpowered, this.Owner);
+    }
+}
