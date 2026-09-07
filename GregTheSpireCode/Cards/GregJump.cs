5335ff9f8b49abbfc0834f5142cf565305a5a48e
UndyingApple <undyingapple@gmail.com>
2026-09-07 14:14:33 -0500

    reworked greg jump

--- GregTheSpireCode/Cards/GregJump.cs
+++ GregTheSpireCode/Cards/GregJump.cs
@@ -1,15 +1,17 @@
 using BaseLib.Extensions;
+using GregTheSpire.GregTheSpireCode.CardPiles;
 using GregTheSpire.GregTheSpireCode.Cards;
 using GregTheSpire.GregTheSpireCode.Powers;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.ValueProps;
 
 namespace GregTheSpire.GregTheSpireCode.Cards;
 
-public class GregJump() : GregTheSpireCard(1,
+public class GregJump() : GregTheSpireCard(0,
     CardType.Skill, CardRarity.Common,
     TargetType.Self)
 {
@@ -17,22 +19,23 @@ public class GregJump() : GregTheSpireCard(1,
 
     protected override IEnumerable<DynamicVar> CanonicalVars =>
     [
-        new BlockVar(7, ValueProp.Move)
     ];
     
-    public override bool GainsBlock => true;
+
 
     protected override async Task OnPlay(
         PlayerChoiceContext choiceContext,
         CardPlay play)
     {
-        await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, play);
-
-        await PowerCmd.Apply<ConfidencePower>(choiceContext, Owner.Creature, 1, Owner.Creature, this);
+        foreach (CardModel card in StashCardPile.StashPileType.GetPile(Owner).Cards.ToList<CardModel>())
+        {
+            await CardPileCmd.Add(card, PileType.Hand);
+        }
+        if(IsUpgraded) await CardPileCmd.Draw(choiceContext, 1, this.Owner);
     }
 
     protected override void OnUpgrade()
     {
-        DynamicVars.Block.UpgradeValueBy(3);
+
     }
 }
