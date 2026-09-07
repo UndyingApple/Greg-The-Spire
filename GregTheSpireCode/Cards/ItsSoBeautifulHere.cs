c2d2381faea229cc2ea3509c8efe941c14b77d96
UndyingApple <undyingapple@gmail.com>
2026-09-07 13:17:10 -0500

    Fixed Tail Swipe rework and started It's So Beautiful Here

--- GregTheSpireCode/Cards/ItsSoBeautifulHere.cs
+++ GregTheSpireCode/Cards/ItsSoBeautifulHere.cs
@@ -1,6 +1,7 @@
 ﻿using GregTheSpire.GregTheSpireCode.CardPiles;
 using GregTheSpire.GregTheSpireCode.Cards;
 using GregTheSpire.GregTheSpireCode.Commands;
+using GregTheSpire.GregTheSpireCode.Powers;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Creatures;
@@ -17,30 +18,36 @@ public class ItsSoBeautifulHere() : GregTheSpireCard(2,
     CardType.Skill, CardRarity.Rare,
     TargetType.Self)
 {
-    protected override IEnumerable<DynamicVar> CanonicalVars => [];
     
-    
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [
+        CardKeyword.Exhaust
+    ];
 
     protected override async Task OnPlay(
         PlayerChoiceContext choiceContext,
         CardPlay play)
     {
         await CreatureCmd.TriggerAnim(this.Owner.Creature, "Cast", this.Owner.Character.CastAnimDelay);
-        if (this.IsUpgraded)
-        {
-            await StashCmd.StashAsync(choiceContext, this.Owner, 0, 3, this);
-        }
+
+        //exhausts cards in your stash (THIS PART WORKS)
         List<CardModel> list1 = StashCardPile.StashPileType.GetPile(this.Owner).Cards.ToList<CardModel>();
-        int exhaustCount = list1.Count;
         foreach (CardModel card in list1)
         {
             CardPileAddResult? nullable = await CardCmd.Exhaust(choiceContext, card);
         }
-        List<CardModel> list2 = CardFactory.GetForCombat(this.Owner, this.Owner.Character.CardPool.GetUnlockedCards(this.Owner.UnlockState, this.Owner.RunState.CardMultiplayerConstraint), exhaustCount, this.Owner.RunState.Rng.CombatCardGeneration).ToList<CardModel>();
-         
-      
-        IReadOnlyList<CardPileAddResult> combat = await CardPileCmd.AddGeneratedCardsToCombat((IEnumerable<CardModel>) list2, StashCardPile.StashPileType, this.Owner);
-            
+        
+        List<CardModel> list2 = CardFactory.GetForCombat(this.Owner, this.Owner.Character.CardPool.GetUnlockedCards(
+            this.Owner.UnlockState, this.Owner.RunState.CardMultiplayerConstraint), Owner.Creature.GetPowerAmount<ConfidencePower>(), this.Owner.RunState.Rng.CombatCardGeneration).ToList<CardModel>();
+
+        if (this.IsUpgraded)
+            CardCmd.Upgrade((IEnumerable<CardModel>) list2, CardPreviewStyle.None);
+        
+        for (int i = 0; i < Owner.Creature.GetPowerAmount<StoragePower>(); i++)
+        {
+            await CardPileCmd.AddGeneratedCardsToCombat((IEnumerable<CardModel>)list2, StashCardPile.StashPileType, this.Owner);
+        }
+
+        //autoplays generated cards
         List<CardModel> list3 = StashCardPile.StashPileType.GetPile(Owner).Cards.ToList<CardModel>();
         int playCount = list3.Count;
         var flag = true;
@@ -51,16 +58,7 @@ public class ItsSoBeautifulHere() : GregTheSpireCard(2,
             flag = false;
         }
     }
-    public override async void ModifyShuffleOrder(
-        Player player,
-        List<CardModel> cards,
-        bool isInitialShuffle)
-    {
-        if (isInitialShuffle || !cards.Contains(this))
-            return;
-        cards.Remove(this);
-        cards.Add(this);
-    }
+    
     protected override void OnUpgrade()
     {
 
