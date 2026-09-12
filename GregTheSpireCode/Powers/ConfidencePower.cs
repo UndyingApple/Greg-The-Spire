6bdac26a7475b3f83a4492152ac17f9da53cc2f0
sockswithafox <sockswithafox@gmail.com>
2026-09-12 13:05:31 -0500

    cofidenc power change

--- GregTheSpireCode/Powers/ConfidencePower.cs
+++ GregTheSpireCode/Powers/ConfidencePower.cs
@@ -16,6 +16,7 @@ public class ConfidencePower() : GregTheSpirePower
 
     public override PowerStackType StackType =>
         PowerStackType.Counter;
+    
 
     public override bool AllowNegative => true;
     
@@ -31,8 +32,7 @@ public class ConfidencePower() : GregTheSpirePower
     public override async Task AfterDamageReceived(PlayerChoiceContext choiceContext, Creature target, DamageResult result, ValueProp props,
         Creature? dealer, CardModel? cardSource)
     {
-                   
-        if (!CombatManager.Instance.IsInProgress || target != this.Owner || result.UnblockedDamage <= 0)
+        if (!CombatManager.Instance.IsInProgress || target != this.Owner || result.UnblockedDamage <= 0 )
             return;
         if (Owner.GetPowerAmount<ObliviousPower>() > 0)
             return;
@@ -40,6 +40,8 @@ public class ConfidencePower() : GregTheSpirePower
         {
             this.Flash();
             await PowerCmd.Apply<ConfidencePower>(choiceContext, this.Owner, -Amount, this.Owner,null);
+            await PowerCmd.Apply<WeakPower>(choiceContext, this.Owner,(int) (1), this.Owner, null);
+                
             return;
         };
         this.Flash(); 
