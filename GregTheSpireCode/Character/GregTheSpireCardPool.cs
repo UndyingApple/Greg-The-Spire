c282f9755b1c6656f943296302dd0f9e7bd2ef5d
sniveeey <thaddeusf1208@gmail.com>
2026-09-12 14:36:13 -0500

    "Fixed" energy (it spins now :))

--- GregTheSpireCode/Character/GregTheSpireCardPool.cs
+++ GregTheSpireCode/Character/GregTheSpireCardPool.cs
@@ -8,8 +8,8 @@ public class GregTheSpireCardPool : CustomCardPoolModel
 {
     public override string Title => GregTheSpire.CharacterId; //This is not a display name.
 
-    public override string BigEnergyIconPath => "charui/big_energy.png".ImagePath();
-    public override string TextEnergyIconPath => "charui/text_energy.png".ImagePath();
+    public override string BigEnergyIconPath => "charui/greg_energy.png".ImagePath();
+    public override string TextEnergyIconPath => "charui/small_greg_energy.png".ImagePath();
 
 
     /* These HSV values will determine the color of your card back.
