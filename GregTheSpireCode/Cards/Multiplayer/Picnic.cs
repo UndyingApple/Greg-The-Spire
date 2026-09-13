68494e3b9b59e510c714e706b08cc354a3352dbb
sockswithafox <sockswithafox@gmail.com>
2026-09-13 13:21:42 -0500

    3 new multipayer cards

---
+++ GregTheSpireCode/Cards/Multiplayer/Picnic.cs
@@ -0,0 +1,53 @@
+﻿using GregTheSpire.GregTheSpireCode.Cards;
+using GregTheSpire.GregTheSpireCode.Cards.Colorless;
+using GregTheSpire.GregTheSpireCode.Commands;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Factories;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+
+namespace GregTheSpire.GregTheSpireCode.Cards.Multiplayer;
+
+public class Picnic() : GregTheSpireCard(2,
+    CardType.Skill, CardRarity.Uncommon,
+    TargetType.AllAllies)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new IntVar("snackAmount", 2)
+    ];
+    public IEnumerable<CardModel> SnackTokens =>
+    [
+        ModelDb.Card<Strawberry>(),
+        ModelDb.Card<Soda>(),
+        ModelDb.Card<Olive>(),
+        ModelDb.Card<Cheese>(),
+        ModelDb.Card<Cracker>(),
+        ModelDb.Card<Fly>(),
+        ModelDb.Card<Soup>()
+    ];
+    public override CardMultiplayerConstraint MultiplayerConstraint
+    {
+        get => CardMultiplayerConstraint.MultiplayerOnly;
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        
+        foreach (Creature creature in (IEnumerable<Creature>) this.CombatState.PlayerCreatures.Where<Creature>((Func<Creature, bool>) (c => c != null && c.IsAlive)).ToList<Creature>())
+        {
+            for (int index = 0; index < DynamicVars["StealAmount"].IntValue; ++index)
+                await CardPileCmd.AddGeneratedCardsToCombat(
+                    (IEnumerable<CardModel>)CardFactory
+                        .GetDistinctForCombat(creature.Player, SnackTokens, 1, Owner.RunState.Rng.CombatCardGeneration)
+                        .ToList<CardModel>(), PileType.Hand, creature.Player);
+        }
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars["snackAmount"].UpgradeValueBy(1);
+    }
+}
---
+++ GregTheSpireCode/Powers/SharingPower.cs
@@ -0,0 +1,40 @@
+﻿using GregTheSpire.GregTheSpireCode.Keywords;
+using GregTheSpire.GregTheSpireCode.Powers;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.Models;
+
+namespace GregTheSpire.GregTheSpireCode.Powers;
+
+public class SharingPower() : GregTheSpirePower
+{
+    private bool _isAddingSnack;
+
+    public override PowerType Type =>
+        PowerType.Buff;
+
+    public override PowerStackType StackType =>
+        PowerStackType.Counter;
+
+    private bool IsAddingSnack
+    {
+        get => this._isAddingSnack;
+        set
+        {
+            this.AssertMutable();
+            this._isAddingSnack = value;
+        }
+    }
+
+    public override async Task AfterCardGeneratedForCombat(CardModel card, Player? creator)
+    {
+        if (creator == null || creator.Creature != this.Applier || !card.Keywords.Contains(GregTheSpireKeywords.Snack) || this.IsAddingSnack)
+            return;
+        this.IsAddingSnack = true;
+        this.Flash();
+        CardCmd.PreviewCardPileAdd(await CardPileCmd.AddGeneratedCardToCombat(card.CreateClone(), PileType.Draw, this.Owner.Player, CardPilePosition.Random));
+        this.IsAddingSnack = false;
+    }
+}
